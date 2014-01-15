f1a751bf8a725b9bd4ba0f09c70fe2d0050e6be8
narasace <narasace@gmail.com>
2014-01-15 00:00:34 -0800

    source for voting game

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ votegame
 ========
 
 a simple vote game
+
+========
+
+local: node/gitvote
+
+aws: votegame
+
+========
+
+being tested on aws now.
+
+mapped domain: webtank.ca
+
+========
---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var app = express();
+
+require('./config')(app);
+
+require('./routes')(app);
+
+app.listen(8080);
+console.log('Your application is running on http://localhost:8080');
